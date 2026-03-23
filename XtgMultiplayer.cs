5d76b9807216838e1fa3d8b6dbe546932cc06ed0
Amitai <git@amitai.ga>
2026-03-23 21:29:52 +0000

    fix input block during recovery

--- XtgMultiplayer.cs
+++ XtgMultiplayer.cs
@@ -43,7 +43,26 @@ namespace XtgMultiplayer
                 return true;
             }
         }
-        
+
+        [HarmonyPatch(typeof(SpaceTurtle_FallRecover), "Initialize")]
+        static class UnblockOtherPlayerWhenRecovering
+        {
+            public static bool Prefix(ref Character carrying)
+            {
+                PlayerController.InputBlockStackCount--;
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerController), "IsInputAvailable", MethodType.Getter)]
+        static class BlockThisPlayerWhenRecovering
+        {
+            public static void Postfix(PlayerController __instance, ref bool __result)
+            {
+                __result = __result || __instance.IsReturning;
+            }
+        }
+
         [HarmonyPatch(typeof(GameManager), "HandleGameplayStateEndLogic")]
         static class DestroyPlayersAfterRun
         {
