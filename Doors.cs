919eea1d2b3a4dda2a3aa18a45233aa052893b0b
Amitai <git@amitai.ga>
2026-04-09 20:36:50 +0100

    prevent exiting multiple doors
    Co-authored-by: Fraser <fraserfallows@hotmail.co.uk>

---
+++ Doors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace XtgMultiplayer
+{
+    static class Doors
+    {
+        static bool isExitingDoor = false;
+
+        [HarmonyPatch(typeof(DoorBase), "ExitTheDoor")]
+        static class SetExiting
+        {
+            public static bool Prefix()
+            {
+                isExitingDoor = true;
+                return true;
+            }
+        }
+
+        //[HarmonyPatch(typeof(DoorBase), "OnEnterAnimationCompleted")]
+        [HarmonyPatch(typeof(Character), "Show", new Type[] { typeof(Vector2) })]
+        static class UnsetExiting
+        {
+            public static void Postfix(Character __instance)
+            {
+                if (__instance.Type == CharacterType.Player)
+                {
+                    isExitingDoor = false;
+                }
+            }
+        }
+
+        //[HarmonyPatch(typeof(PlayerController), "Interact", MethodType.Getter)]
+        [HarmonyPatch(typeof(InteractiveObject), "InteractionEnabled", MethodType.Getter)]
+        static class StopInteraction
+        {
+            public static void Postfix(ref bool __result)
+            {
+                __result = __result && !isExitingDoor;
+            }
+        }
+
+    }
+}
